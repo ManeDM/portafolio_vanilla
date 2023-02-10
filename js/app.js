2a2871846e04b35e6f9ff95a149bb4d9ad885b52
ManeDM <mane657@hotmail.com.com>
2023-02-10 18:30:26 -0500

    adding menu

--- js/app.js
+++ js/app.js
@@ -9,3 +9,16 @@ function parallax(e) {
         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
     });
 }
+
+// selector
+var menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu (event) {
+  this.classList.toggle('is-active');
+  document.querySelector( ".box_2" ).classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
